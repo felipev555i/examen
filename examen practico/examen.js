101c3ecb78a3b3d7591d2c08098c5e7fd9a3c86a
felipev555i <132092415+felipev555i@users.noreply.github.com>
2023-06-16 11:05:48 -0500

    Add files via upload

---
+++ examen practico/examen.js
@@ -0,0 +1,33 @@
+var utensiliosEscolares = [
+    {
+      nombre: "Lápiz",
+      color: "Amarillo",
+      tipo: "HB",
+      precio: 0.5,
+      cantidad: 10
+    },
+    {
+      nombre: "Bolígrafo",
+      color: "Azul",
+      tipo: "Gel",
+      precio: 1.2,
+      cantidad: 5
+    },
+    {
+      nombre: "Cuaderno",
+      color: "Verde",
+      tipo: "Rayado",
+      precio: 2.5,
+      cantidad: 3
+    },
+    {
+      nombre: "Borrador",
+      color: "Blanco",
+      tipo: "Plástico",
+      precio: 0.8,
+      cantidad: 8
+    }
+  ];
+  
+console.log(utensiliosEscolares);
+  
